e14e4893eeb29b3e608aab6be059f8e2d27178ba
Maksim <maksimchilout@gmail.com>
2023-08-13 14:01:00 +0300

    9_les

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <header className='header'>
+        <img className='logo-roto' src='./logo192.png' alt='logo' />
+        </header>
+    )
+}
+
+export default Header
---
+++ src/components/Profile.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Profile = () => {
+    return (
+        <div className='content'>
+        <div>
+          <img src='./image-main.jpg' alt='main-im' />
+        </div>
+        <div>
+          Ava + description
+        </div>
+        <div>
+          My posts
+          <div>
+            new posts
+          </div>
+          <div>
+            post 1
+          </div>
+          <div>
+            post 2
+          </div>
+        </div>
+      </div>
+    )
+}
+
+export default Profile
