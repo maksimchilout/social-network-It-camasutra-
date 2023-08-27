2d0d6588684864b5575b2e0b13baef4a3e07b32a
Maksim <maksimchilout@gmail.com>
2023-08-27 16:18:48 +0300

    lesson_Fifty_Three

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -2,65 +2,40 @@ import React from "react";
 import styles from './users.module.css'
 import axios from "axios";
 
+class Users extends React.Component{
 
-let Users = (props) => {
- if (props.users.length === 0) {
+    constructor(props) {
+        super(props)
+        if (this.props.users.length === 0) {
+            console.log(this.props.users.length)
+            axios.get('https://social-network.samuraijs.com/api/1.0/users')
+                .then(response => {
+                    // debugger
+                    // console.log(response.data.items)
+                    this.props.setUsers(response.data.items)
+                })
+        }
+    }
 
-     axios.get('https://social-network.samuraijs.com/api/1.0/users')
-         .then(response => {
-             // debugger
-             // console.log(response.data.items)
-             props.setUsers(response.data.items)
-         })
-     //
-     // props.setUsers(
-     //     [
-     //         {
-     //             id: 1,
-     //             photoURL: "./image/racon.jpg",
-     //             followed: false,
-     //             fullName: 'Dmitry',
-     //             status: 'I am a boss',
-     //             location: {city: 'Minsk', country: 'Belarus'}
-     //         },
-     //         {
-     //             id: 2,
-     //             photoURL: "../image/girl.jpg",
-     //             followed: true,
-     //             fullName: 'Kristina',
-     //             status: 'I am a boss too',
-     //             location: {city: 'Moscow', country: 'Russia'}
-     //         },
-     //         {
-     //             id: 3,
-     //             photoURL: "./image/minion.jpg",
-     //             followed: false,
-     //             fullName: 'Andrew',
-     //             status: 'I am a row employee',
-     //             location: {city: 'Kiev', country: 'Ukraine'}
-     //         }
-     //     ]
-     // )
- }
-
-    return (
-        <div>
-            {
-                props.users.map(u => {
-                    return (
-                        <div key={u.id}>
+    render() {
+        return (
+            <div>
+                {
+                    this.props.users.map(u => {
+                        return (
+                            <div key={u.id}>
                             <span>
                                 <div>
-                                    <img className={styles.userPhoto} src={ u.photos.small != null ?u.photos.small : './image/minion.jpg' } alt=""/>
+                                    <img className={styles.userPhoto} src={ u.photos.small != null ? u.photos.small : './image/minion.jpg' } alt=""/>
                                 </div>
                                 <div>
                                     {u.followed
-                                        ? <button onClick={() => {props.unfollow(u.id)}}>Unfollow</button>
-                                        : <button onClick={() => {props.follow(u.id)}}>Follow</button>}
+                                        ? <button onClick={() => {this.props.unfollow(u.id)}}>Unfollow</button>
+                                        : <button onClick={() => {this.props.follow(u.id)}}>Follow</button>}
 
                                 </div>
                             </span>
-                            <span>
+                                <span>
                                 <span>
                                     <div>{u.name}</div>
                                     <div>{u.status}</div>
@@ -70,11 +45,12 @@ let Users = (props) => {
                                     <div>{"u.location.city"}</div>
                                 </span>
                             </span>
-                        </div>
-                    )
-                })
-            }
-        </div>
-    )
+                            </div>
+                        )
+                    })
+                }
+            </div>
+        )
+    }
 }
 export default Users
