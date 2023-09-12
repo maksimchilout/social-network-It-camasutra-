59e178bf3d4d00495a452a9dde0450a2f1274562
Maksim <maksimchilout@gmail.com>
2023-09-12 18:52:45 +0300

    lesson_Eighteen

---
+++ src/components/commons/preloader/PreloaderInfinite.js
@@ -0,0 +1,13 @@
+import preloader from "../../../images/Spinner-1s-200px.svg";
+import React from "react";
+
+
+let Preloader = (props) => {
+    return(
+        <div>
+            {props.isFetching ? <img src={preloader} alt='preloader'/> : <img src={preloader} alt='preloader'/>}
+        </div>
+    )
+}
+
+export default Preloader
