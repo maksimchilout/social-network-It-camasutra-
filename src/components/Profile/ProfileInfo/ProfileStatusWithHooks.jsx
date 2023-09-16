ad94b0e424cc777d89edd2fad898baf492da08ce
Maksim <maksimchilout@gmail.com>
2023-09-16 08:38:20 +0300

    lesson_Eighty_Five

--- src/components/Profile/ProfileInfo/ProfileStatusWithHooks.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatusWithHooks.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 
 
 const ProfileStatusWithHooks = (props) => {
@@ -7,6 +7,10 @@ const ProfileStatusWithHooks = (props) => {
     // let [editMode, setEditMode] = stateWithSetState
     let [editMode, setEditMode] = useState(false)
     let [status, setStatus] = useState(props.status)
+
+    useEffect(() => {
+        setStatus(props.status)
+    }, [props.status]);
 
     const activateEditMode = () => setEditMode(true)
     const deactivateEditMode = () => {
